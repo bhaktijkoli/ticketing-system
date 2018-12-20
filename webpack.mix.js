017ac8d76630db7c1f7854981edf586136e474dc
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-12-20 12:11:59 +0530

    Prepared structure for assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,19 @@
 const mix = require('laravel-mix');
 
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+|--------------------------------------------------------------------------
+| Mix Asset Management
+|--------------------------------------------------------------------------
+|
+| Mix provides a clean, fluent API for defining some Webpack build steps
+| for your Laravel application. By default, we are compiling the Sass
+| file for the application as well as bundling up all the JS files.
+|
+*/
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/core.js', 'public/js')
+.js('resources/js/app.js', 'public/js')
+.js('resources/js/dashboard.js', 'public/js')
+.sass('resources/sass/core.scss', 'public/css')
+.sass('resources/sass/app.scss', 'public/css')
+.sass('resources/sass/dashboard.scss', 'public/css');
