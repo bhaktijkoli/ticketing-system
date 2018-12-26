84c787c28675665d0ea94a241cfb158bfd85b5e7
Pariharpp <pariharprahalad26@gmail.com>
2018-12-26 13:57:39 +0530

    Small changes

---
+++ resources/js/dashboard.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+window.Vue = require('vue');
+import VueRouter from 'vue-router'
+import Dashboard from './dashboard/Dashboard'
+import AllTicket from './dashboard/components/AllTicket.vue'
+import Tbody from './dashboard/components/Tbody.vue'
+// Vue.component('Dashboard', require('./dashboard/Dashboard'));
+
+Vue.use(VueRouter);
+
+
+const router = new VueRouter({
+    routes: [{
+        path: '/home',
+        name: 'Dashboard',
+        component: Dashboard,
+        children: [{
+                path: '/inbox',
+                name: 'AllTicket',
+                component: AllTicket
+            },
+            {
+                path: 'ticket',
+                name: 'Tbody',
+                component: Tbody
+            }
+        ]
+    }],
+    mode: 'history'
+});
+
+const dashboard = new Vue({
+    router: router,
+    el: '#dashboard'
+});
