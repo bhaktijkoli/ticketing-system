30d6c08d94c1f13422fcfdc368d3a54962716e58
Parag Gawde <paraggawde.303@gmail.com>
2018-12-26 13:51:30 +0530

    Small changes

---
+++ resources/js/app/Login/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <!-- logo -->
+    <div class="login-box">
+      <div class="login-box-body">
+        <p class="login-box-msg">Sign in to start your session</p>
+        <form @submit.prevent="authUser(email, password)" method="post">
+          <div class="form-group has-feedback">
+            <span class="fa fa-envelope-o form-control-feedback"></span>
+            <input v-model="email" type="email" class="form-control" placeholder="Email">
+          </div>
+          <div class="form-group has-feedback">
+            <span class="fa fa-lock form-control-feedback"></span>
+            <input v-model="password" type="password" class="form-control" placeholder="Password">
+          </div>
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <div class="mt-2 passwd">
+            <a href="#">I forgot my password</a>
+          </div>
+          <br>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    };
+  },
+  methods: {
+    authUser: function(email, password) {
+      axios
+        .post("api/auth/login")
+        .then(response => {
+          // this.email = response.data.email;
+          // this.password = response.data.password;
+          console.log(response);
+        })
+        .catch(error => {
+          // this.error = error;
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+<style scoped>
+.passwd {
+  text-align: center;
+}
+</style>
+
+
---
+++ resources/js/dashboard/components/Tbody.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Ticket
+        <small>#007612</small>
+      </h1>
+    </section>
+    <!-- Main content -->
+    <section class="invoice">
+      <!-- title row -->
+      <div class="row">
+        <div class="col-xs-12">
+          <h2 class="page-header">
+            <i class="fa fa-hand-o-right"></i> Subject
+            <small class="pull-right">Date: 2/10/2014</small>
+          </h2>
+        </div>
+        <!-- Ticket details -->
+        <div class="detail-view ticket-body">
+          <h3>Details</h3>
+        </div>
+        <!-- Action btn -->
+        <div class="row no-print">
+          <div class="col-xs-12">
+            <span>action btns</span>
+            <br>
+            <a href="invoice-print.html" target="_blank" class="btn btn-default">
+              <i class="fa fa-print"></i> Print
+            </a>
+            <button type="button" class="btn btn-success pull-right">
+              <i class="fa fa-credit-card"></i> Submit Payment
+            </button>
+            <button type="button" class="btn btn-primary pull-right" style="margin-right: 5px;">
+              <i class="fa fa-download"></i> Generate PDF
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+    <div class="clearfix"></div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Tbody"
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Lora" rel="stylesheet');
+
+h1,
+h2,
+h3 {
+  font-family: "Lora", serif;
+}
+</style>
+
