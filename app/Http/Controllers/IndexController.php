da8f0abf59ca35108eca706f449d3314661a0a59
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-12-27 15:53:50 +0530

    Added guest middleware to Index Controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+  public function __construct() {
+    $this->middleware('guest');
+  }
   public function getLogin() {
     return view('login');
   }
