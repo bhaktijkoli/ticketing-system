286cdfe6daab87c380133499cec0ef66bcfb7143
Parag Gawde <paraggawde.303@gmail.com>
2019-01-03 20:05:17 +0530

    Added pusher-js and laravel-echo in a seperate file

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -1,15 +1,4 @@
-
-import Echo from "laravel-echo"
-
-window.Pusher = require('pusher-js');
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'fe3a2253e56fd44acfa8',
-    cluster: "ap2",
-    encrypted: true,
-});
-
+import pusher from './echo.js'  //Pusher and Laravel-Echo
 import Vue from 'vue';
 window.Vue = require('vue');
 import VueRouter from 'vue-router'
