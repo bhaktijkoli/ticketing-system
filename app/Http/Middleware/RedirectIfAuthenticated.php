0ecb4e7d852efbd546d25fd1ddc5b77dca0602c9
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-01-11 12:46:37 +0530

    Fix auth redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+          if(Auth::user()->role == 0) {
             return redirect('/home');
+          } else {
+            return redirect('/my-tickets');
+          }
         }
 
         return $next($request);
